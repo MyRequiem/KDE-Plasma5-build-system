e0bac2b4f2b8beebf54695b2838b13a81efe055f
MyRequiem <mailto:mrvladislavovich@gmail.com>
2017-03-20 12:22:13 +0400

    remove pre-install/kdepimlibs4.sh

--- pre-install/kdepimlibs4.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# add "cmake_minimum_required(VERSION 2.8.9)" string
-# at the top of CMakeLists.txt
-if ! grep -q 'cmake_minimum_required' CMakeLists.txt; then
-    sed -i -e "1icmake_minimum_required(VERSION 2.8.9)" CMakeLists.txt
-fi
