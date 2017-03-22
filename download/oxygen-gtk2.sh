e7247989e2c375e91a36298d1de0418f9f84351a
MyRequiem <mailto:mrvladislavovich@gmail.com>
2017-03-22 10:46:05 +0400

    added kde4-extragear: oxygen-gtk2, oxygen-gtk3, kwebkitpart

--- download/oxygen-gtk2.sh
+++ download/oxygen-gtk2.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
 cd "${SRCDIR}/${MODULE}" || exit 1
-rm -rf "${PKGNAME}"
-git clone "git://anongit.kde.org/kwebkitpart.git" 1>/dev/null 2>&1
-cd "${PKGNAME}" || exit 1
-VERSION=$(git tag | tr -d "v" | sort -V | tail -n 1)
-export VERSION
+SRCDIRNAME=$(echo "${PKGNAME}" | tr -d 2)
+rm -rf "${SRCDIRNAME}"
+git clone "git://anongit.kde.org/oxygen-gtk.git" 1>/dev/null 2>&1
+cd "${SRCDIRNAME}" || exit 1
+VERSION=$(git tag | grep -v gtk3 | tr -d "v" | sort -V | tail -n 1)
 cd .. || exit 1
 
+export VERSION
 echo "${VERSION}"
 PKGVER="${PKGNAME}-${VERSION}"
 export SOURCE="${PKGVER}.tar.xz"
 
 if ! [ -r "${SOURCE}" ]; then
-    cd "${PKGNAME}" || exit 1
+    mv "${SRCDIRNAME}" "${PKGVER}"
+    cd "${PKGVER}" || exit 1
     COMMIT=$(git show "v${VERSION}" | grep "commit " | cut -d " " -f 2)
     git reset --hard "${COMMIT}" 1>/dev/null 2>&1
     cd .. || exit 1
-    rm -rf "${PKGNAME}/.git"
-    mv "${PKGNAME}" "${PKGVER}"
+    rm -rf "${PKGVER}/.git"
     tar -cJf "${PKGVER}.tar.xz" "${PKGVER}" 1>/dev/null
     rm -rf "${PKGVER}"
 else
-    rm -rf "${PKGNAME}"
+    rm -rf "${SRCDIRNAME}"
 fi
 
 cd "${CWD}" || exit 1
---
+++ download/oxygen-gtk3.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+cd "${SRCDIR}/${MODULE}" || exit 1
+SRCDIRNAME=$(echo "${PKGNAME}" | tr -d 3)
+rm -rf "${SRCDIRNAME}"
+git clone "git://anongit.kde.org/oxygen-gtk.git" 1>/dev/null 2>&1
+cd "${SRCDIRNAME}" || exit 1
+VERSION=$(git tag | grep vgtk3 | cut -d - -f 2 | sort -V | tail -n 1)
+cd .. || exit 1
+
+export VERSION
+echo "${VERSION}"
+PKGVER="${PKGNAME}-${VERSION}"
+export SOURCE="${PKGVER}.tar.xz"
+
+if ! [ -r "${SOURCE}" ]; then
+    mv "${SRCDIRNAME}" "${PKGVER}"
+    cd "${PKGVER}" || exit 1
+    COMMIT=$(git show "vgtk3-${VERSION}" | grep "commit " | cut -d " " -f 2)
+    git reset --hard "${COMMIT}" 1>/dev/null 2>&1
+    cd .. || exit 1
+    rm -rf "${PKGVER}/.git"
+    tar -cJf "${PKGVER}.tar.xz" "${PKGVER}" 1>/dev/null
+    rm -rf "${PKGVER}"
+else
+    rm -rf "${SRCDIRNAME}"
+fi
+
+cd "${CWD}" || exit 1
