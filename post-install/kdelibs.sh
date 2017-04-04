71a30ae6ecf9a54508988a56ddd055b1febbf794
MyRequiem <mailto:mrvladislavovich@gmail.com>
2017-04-04 23:37:27 +0400

    added some post-install scripts

--- post-install/kdelibs.sh
+++ post-install/kdelibs.sh
@@ -3,3 +3,7 @@
 # move the polkit dbus configuration files to the proper place
 DBUS="${PKG}/etc/kde/dbus-1"
 [ -d "${DBUS}" ] && mv "${DBUS}" "${PKG}/etc/"
+
+# remove html docs
+HTML="${PKG}/usr/doc/${PKGNAME}-${VERSION}/HTML"
+[ -d "${HTML}" ] && rm -rf "${HTML}"
--- post-install/kdepimlibs4.sh
+++ post-install/kdepimlibs4.sh
@@ -3,3 +3,7 @@
 # remove files that clash with the Frameworks version of kdepimlibs
 rm -rf "${PKG}/usr/bin"
 rm -rf "${PKG}/usr/share/config.kcfg"
+
+# remove html docs
+HTML="${PKG}/usr/doc/${PKGNAME}-${VERSION}/HTML"
+[ -d "${HTML}" ] && rm -rf "${HTML}"
